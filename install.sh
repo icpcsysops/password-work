4dd86388f2d5fe03d68f1890420ce59ff1c76237
Nicky Gerritsen <nickygerritsen@me.com>
2023-06-16 11:55:58 +0200

    Initial changes moving to a config file, does not generate anything yet.

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 # Purpose: install required python modules
 #
 
-sudo apt install python3-yaml python3-jinja2 python3-pdfkit xkcdpass wkhtmltopdf
+sudo apt install python3-yaml python3-pip python3-jinja2 python3-pdfkit xkcdpass wkhtmltopdf
+sudo pip3 install questionary
