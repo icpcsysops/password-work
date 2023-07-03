35d827335dda0aa9e19c80c6e91aedca44252f84
Douglas A. Lane <laned@csus.edu>
2023-07-03 12:58:05 -0700

    test01 removed config/data files from test01
    Files will be copied from source before test is done
    to ensure that the latest files are used.

--- sample/test01/gen/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Purpose: install required python modules
-#
-
-sudo apt install python3-yaml python3-pip python3-jinja2 python3-pdfkit xkcdpass wkhtmltopdf
-sudo pip3 install questionary
